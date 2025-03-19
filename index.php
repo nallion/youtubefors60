4216a8031e57c56a15506deaa7e1650c017c5bc5
nallion <97355641+nallion@users.noreply.github.com>
2025-03-19 04:22:32 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
-Symbian youtube client.<br><br>
+Symbian youtube client. Youtube to rtsp gateway.<br><br>
 <title>Youtube</title>
 <form action="index.php" method="POST">
 Поиск видеороликов на Youtube: <input type="text" name="videoname">
-<input type="submit">
+<input type="submit" value="Искать!">
 </form>
 
 <?php
@@ -15,13 +15,14 @@ echo "<br><br>";
 
 $request = $_POST["videoname"];
 if (empty($request)){ die(); }
+file_put_contents('/var/www/html/reqlog.txt', $request . "\r\n", FILE_APPEND);
 $reqenc = urlencode($request);
 $ids = shell_exec("curl -A 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36' 'https://www.youtube.com/results?search_query=$reqenc' | grep -oP '(?<=\"videoId\":\"|\"videoId\":\\\")\\w{11}' | uniq");
 $idsarray = preg_split('/\s+/', trim($ids));
 foreach ($idsarray as $item) {
-  if (++$i == 10) break;
+  if (++$i == 11) break;
   $videon = shell_exec("curl -s -A 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36' 'https://www.youtube.com/watch?v=$item' | grep -o -P '(?<=<title>).*(?=</title>)' | sed 's/- YouTube//g'");
-  echo $videon;
+  echo "<font color=blue><a href='stream.php?id=$item'>$videon</font></a>";
   echo "<br>";
   echo "<a href='stream.php?id=$item'><img src=https://i.ytimg.com/vi/$item/1.jpg></a><br>";
   }
--- stream.php
+++ stream.php
@@ -1,6 +1,10 @@
 <?php
 $idstream = $_GET["id"];
-exec("/usr/bin/nohup /var/www/html/yt-dlp_linux https://www.youtube.com/watch?v=$idstream -o - | ffmpeg -re -i - -acodec amr_wb -ar 16000 -ac 1 -vcodec h263 -vb 70k -r 15 -vf scale=176:144 -f rtsp rtsp://127.0.0.1:80/$idstream >/dev/null 2>&1 &");
-sleep(12);
+//kill old worker
+$existpid = shell_exec("pgrep -f 'ffmpeg.*$idstream'");
+exec("kill $existpid");
+//create new one
+exec("/usr/bin/nohup /var/www/html/yt-dlp_linux https://www.youtube.com/watch?v=$idstream -o - | ffmpeg -re -i - -acodec amr_wb -ar 16000 -ac 1 -vcodec h263 -vb 70k -r 15 -vf scale=176:144 -f rtsp rtsp://127.0.0.1:80/$idstream >/tmp/yt_dlpdebug.txt 2>&1 &");
+sleep(15);
 header("Location: rtsp://tv.tg-gw.com:80/$idstream");
 echo "<a href=rtsp://tv.tg-gw.com:80/$idstream>Смотреть</a>";
