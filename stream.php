00e976caa1eb73b6f082392712406ced7e0df23d
nallion <97355641+nallion@users.noreply.github.com>
2025-03-28 19:53:15 +0100

    Add checking for alive stream

--- stream.php
+++ stream.php
@@ -5,10 +5,18 @@ if (!preg_match("/^[a-zA-Z0-9_-]{11}$/", $idstream)) { echo 404; die(); }
 $existpid = shell_exec("pgrep -f 'ffmpeg.*$idstream'");
 exec("kill $existpid");
 //create new one
-exec("/usr/bin/nohup /var/www/html/yt-dlp_linux https://www.youtube.com/watch?v=$idstream -o - | ffmpeg -re -i - -acodec amr_wb -ar 16000 -ac 1 -ab 24k -vcodec mpeg4 -vb 104k -r 15 -vf scale=320:240 -f rtsp rtsp://127.0.0.1:8080/$idstream >/tmp/yt_dlpdebug.txt 2>&1 &");
-sleep(20);
-echo "<a href=rtsp://tv.tg-gw.com:554/$idstream>Смотреть (ссылка 1)</a> *554 порт<br>";
-echo "<a href=rtsp://tv.tg-gw.com:443/$idstream>Смотреть (ссылка 2)</a> *443 порт, использовать, если не работает первая<br>";
-echo "<a href=rtsp://tv.tg-gw.com:8080/$idstream>Смотреть (ссылка 3)</a> *8080 порт, использовать, если не работают предыдущие<br>";
-echo "<a href=rtsp://tv.tg-gw.com:8554/$idstream>Смотреть (ссылка 4)</a> *8554 можно попробовать и этот<br>";
+exec("/usr/bin/nohup /var/www/html/yt-dlp_linux https://www.youtube.com/watch?v=$idstream -o - | ffmpeg -re -i - -t 18000 -acodec amr_wb -ar 16000 -ac 1 -ab 24k -vcodec mpeg4 -vb 128k -r 15 -vf scale=320:240 -f rtsp rtsp://127.0.0.1:8080/$idstream >/tmp/yt_dlpdebug.txt 2>&1 &");
+
+streamfound:
+
+$checkstream = exec("ffprobe -show_streams  -v quiet rtsp://127.0.0.1:8080/$idstream");
+if (empty($checkstream)){
+sleep (3);
+goto streamfound;
+}
+
+echo "<a href=rtsp://tv.tg-gw.com:554/$idstream>Watch (link 1)</a> *554 порт<br>";
+echo "<a href=rtsp://tv.tg-gw.com:443/$idstream>Watch (link 2)</a> *443 порт, использовать, если не работает первая<br>";
+echo "<a href=rtsp://tv.tg-gw.com:8080/$idstream>Watch (link 3)</a> *8080 порт, использовать, если не работают предыдущие<br>";
+echo "<a href=rtsp://tv.tg-gw.com:8554/$idstream>Watch (link 4)</a> *8554 можно попробовать и этот<br>";
 echo "<a href=index.php><font color=blue>Назад</font></a>";
